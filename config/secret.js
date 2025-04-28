035e1714bd5f417130a51ff08a3a1ebe78312818
蘇祺元 <suqiyuan@suqiyuande-MacBook-Pro.local>
2025-04-28 21:35:09 +0800

    隱藏firebase

--- config/secret.js
+++ config/secret.js
@@ -1,8 +1,8 @@
 module.exports = {
   jwtSecret: process.env.JWT_SECRET,
   jwtExpiresDay: process.env.JWT_EXPIRES_DAY,
-  firebase: {
-    serviceAccount: require('./firebase.json'),  // <<--- 這行
-    storageBucket: process.env.FIREBASE_STORAGE_BUCKET
-  }
-}
+//   firebase: {
+//     serviceAccount: require('./firebase.json'),  // <<--- 這行
+//     storageBucket: process.env.FIREBASE_STORAGE_BUCKET
+//   }
+ }
